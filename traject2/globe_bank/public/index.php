1b832e1c4c52c47f2cc27e2b67d34ff4f6cab23f
Luc Drenth <23924@ma-web.nl>
2017-12-06 15:00:30 +0100

    Add files via upload

---
+++ traject2/globe_bank/public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>Globe bank</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+<h1>Globe Bank coming soon</h1>
+</body>
+
+</html>
---
+++ traject2/globe_bank/public/staff/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>Globe bank</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="..//stylesheets/staff.css" />
+</head>
+
+<body>
+
+<header>
+</header>
+
+<navigation>
+  <ul>
+    <li><a href="index.php">menu</a></li>
+  </ul>
+</navigation>
+
+<footer>
+  &copy; <?php echo date('Y'); ?> Globe Bank
+</footer>
+
+</body>
+
+</html>
